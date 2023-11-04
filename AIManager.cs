097edd79942f10f497e47ff7a22702aa7f1d2084
Tewodros W. Ambaw <teddy.ww@gmail.com>
2023-11-04 11:05:17 +0200

    Revert "Merge branch 'main' of https://github.com/amteddy/AI2PdfChat"
    This reverts commit 614c430bb26166c77886afb334ccb24d21a14a46, reversing
    changes made to 12b7e77b5d05b2c18b9887d64a51fe194b42f946.

--- AIManager.cs
+++ AIManager.cs
@@ -47,7 +47,7 @@ namespace AI2PdfChat
             {
                 try
                 {
-                    using dynamic py = Py.Import("AIInteractor");
+                    using dynamic py = Py.Import("AI");
                     if (!initialized)
                     {
                         RefreshDocumentImport();
@@ -55,8 +55,8 @@ namespace AI2PdfChat
                     else
                     {
                         if (!string.IsNullOrEmpty(message))
-                        {
-                            result = py.continue_chat(message);
+                        {                               
+                            result = py.chat(message);
                         }
                     }
 
@@ -78,9 +78,9 @@ namespace AI2PdfChat
             {
                 try
                 {
-                    using dynamic py = Py.Import("AIInteractor");
+                    using dynamic py = Py.Import("AI");
                     py.import_all_pdfs_in_directory(pdfLocation);
-                    py.initialize_vector();
+                    py.initialize();
                     initialized = true;
                     status = true;
                 }
